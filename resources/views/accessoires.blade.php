caece49254ab8480bccd7768d20414742909e5d6
Douae sb <douae.sb411@example.com>
2024-04-16 00:28:08 +0100

    details accessoire desc

--- resources/views/accessoires.blade.php
+++ resources/views/accessoires.blade.php
@@ -102,24 +102,41 @@
                                 </h3>
                             </div>
                             <p class="text-slate-400 text-lg p-2">
-                                {{ $product->description }}
+                                
+                                    @foreach (explode('</li><li>', $product ->description) as $item)
+                                        @php
+                                            $trimmedItem = strip_tags($item); 
+                                            $trimmedItem = substr($trimmedItem, 0, 30); 
+                                            $trimmedItem .= strlen($trimmedItem) < strlen($item) ? '...' : ''; 
+                                        @endphp
+                                        <li class="text-slate-400 text-lg p-2 list-disc">{!! $trimmedItem !!}</li>
+                                    @endforeach
+                              
                             </p>
                             <div class="flex justify-between">
 
-                                <button class="border-2 border-amber-300 px-8 p-2 w-fit">Add to cart  </button>
+                                <button class="border-2 border-amber-300 px-8 p-2 w-fit">Add to cart </button>
                                 <a href="{{ route('accessoires.show', ['id' => $product['id']]) }}" class="flex ">
-                                    <svg width='28px' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <svg width='28px' viewBox="0 0 24 24" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
                                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round">
+                                        </g>
                                         <g id="SVGRepo_iconCarrier">
-                                            <path d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z" stroke="#fbbf24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z" stroke="#fbbf24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                                            <path
+                                                d="M15.0007 12C15.0007 13.6569 13.6576 15 12.0007 15C10.3439 15 9.00073 13.6569 9.00073 12C9.00073 10.3431 10.3439 9 12.0007 9C13.6576 9 15.0007 10.3431 15.0007 12Z"
+                                                stroke="#fbbf24" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round"></path>
+                                            <path
+                                                d="M12.0012 5C7.52354 5 3.73326 7.94288 2.45898 12C3.73324 16.0571 7.52354 19 12.0012 19C16.4788 19 20.2691 16.0571 21.5434 12C20.2691 7.94291 16.4788 5 12.0012 5Z"
+                                                stroke="#fbbf24" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round"></path>
                                         </g>
                                     </svg>
                                     <span class="mt-2 ml-1 text-amber-400 underline">
                                         View more
                                     </span>
-                                  
+
                                 </a>
                             </div>
                         </div>
