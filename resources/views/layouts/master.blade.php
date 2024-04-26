98ceee4026d5e0f6fe35e77fa0d64e08f8d18442
Douae sb <douae.sb411@example.com>
2024-04-26 11:06:13 +0100

    icon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('./img/booksyellow.png') }}">
     {{-- <meta http-equiv="X-UA-Compatible" content="ie=edge"> --}}
     <script src="https://cdn.tailwindcss.com"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
